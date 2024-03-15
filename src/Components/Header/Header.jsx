7d08fd82eaf238af1735d9e3a3b83283050b454b
Sadman-Chowdhury <sadman.ch49@gmail.com>
2024-03-15 22:46:35 +0600

    Banner part added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,11 @@
 import { IoIosSearch } from "react-icons/io";
 import { FaRegCircleUser } from "react-icons/fa6";
+import './Header.css'
 const Header = () => {
     return (
-        <div className="flex items-center justify-between">
+        <div>
+            {/* Navbar */}
+            <div className="flex items-center justify-between">
             <div>
             <h1 className="text-3xl font-bold">Recipe Calories</h1>
             </div>
@@ -18,6 +21,20 @@ const Header = () => {
                 <FaRegCircleUser />
                 </button>
             </div>
+        </div>
+        {/* banner */}
+        <div className="custom-background-1 space-y-11 text-center px-[150px] py-[100px] rounded-3xl mt-12">
+            <h1 className="text-[52px] font-bold text-white">Discover an exceptional cooking <br /> class tailored for you!</h1>
+            <p className="text-[18px] text-white">Here we have the recipes of healthy dishes that plays a pivotal role in nourishing the body while maintaining energy balance. They serve as the cornerstone of a balanced diet, fostering vitality and supporting overall well-being.</p>
+            <div className="flex gap-4 justify-center">
+            <button className="text-xl font-semibold bg-[#0BE58A] px-8 py-5 rounded-[50px]">
+               Explore Now
+                </button>
+                <button className="text-xl font-semibold border px-8 py-5 rounded-[50px] text-white">
+               Our Feedback
+                </button>
+            </div>
+        </div>
         </div>
     );
 };
